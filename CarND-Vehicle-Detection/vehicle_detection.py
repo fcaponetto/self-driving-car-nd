cddf20735497ef3f94d903cd5540ec3366ef8fcf
Fernando Caponetto <fernando.cponetto@gmail.com>
2018-04-16 15:23:54 +0200

    update vehicle_detection.py

--- CarND-Vehicle-Detection/vehicle_detection.py
+++ CarND-Vehicle-Detection/vehicle_detection.py
@@ -20,13 +20,12 @@ import imageio
 imageio.plugins.ffmpeg.download()
 
 def get_hog_features(img, orient, pix_per_cell, cell_per_block,
-                        vis=False, feature_vec=True):
+                     vis=False, feature_vec=True):
     # Call with two outputs if vis==True
     if vis == True:
         features, hog_image = hog(img, orientations=orient,
                                   pixels_per_cell=(pix_per_cell, pix_per_cell),
                                   cells_per_block=(cell_per_block, cell_per_block),
-                                  block_norm= 'L2-Hys',
                                   transform_sqrt=False,
                                   visualise=vis, feature_vector=feature_vec)
         return features, hog_image
@@ -35,7 +34,6 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
         features = hog(img, orientations=orient,
                        pixels_per_cell=(pix_per_cell, pix_per_cell),
                        cells_per_block=(cell_per_block, cell_per_block),
-                       block_norm= 'L2-Hys',
                        transform_sqrt=False,
                        visualise=vis, feature_vector=feature_vec)
         return features
@@ -69,52 +67,6 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
         cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
     # Return the image copy with boxes drawn
     return imcopy
-
-
-# Define a function that takes an image,
-# start and stop positions in both x and y,
-# window size (x and y dimensions),
-# and overlap fraction (for both x and y)
-def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
-                 xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
-    # If x and/or y start/stop positions not defined, set to image size
-    if x_start_stop[0] == None:
-        x_start_stop[0] = 0
-    if x_start_stop[1] == None:
-        x_start_stop[1] = img.shape[1]
-    if y_start_stop[0] == None:
-        y_start_stop[0] = 0
-    if y_start_stop[1] == None:
-        y_start_stop[1] = img.shape[0]
-    # Compute the span of the region to be searched
-    xspan = x_start_stop[1] - x_start_stop[0]
-    yspan = y_start_stop[1] - y_start_stop[0]
-    # Compute the number of pixels per step in x/y
-    nx_pix_per_step = np.int(xy_window[0] * (1 - xy_overlap[0]))
-    ny_pix_per_step = np.int(xy_window[1] * (1 - xy_overlap[1]))
-    # Compute the number of windows in x/y
-    nx_buffer = np.int(xy_window[0] * (xy_overlap[0]))
-    ny_buffer = np.int(xy_window[1] * (xy_overlap[1]))
-    nx_windows = np.int((xspan - nx_buffer) / nx_pix_per_step)
-    ny_windows = np.int((yspan - ny_buffer) / ny_pix_per_step)
-    # Initialize a list to append window positions to
-    window_list = []
-    # Loop through finding x and y window positions
-    # Note: you could vectorize this step, but in practice
-    # you'll be considering windows one by one with your
-    # classifier, so looping makes sense
-    for ys in range(ny_windows):
-        for xs in range(nx_windows):
-            # Calculate window position
-            startx = xs * nx_pix_per_step + x_start_stop[0]
-            endx = startx + xy_window[0]
-            starty = ys * ny_pix_per_step + y_start_stop[0]
-            endy = starty + xy_window[1]
-
-            # Append window position to list
-            window_list.append(((startx, starty), (endx, endy)))
-    # Return the list of windows
-    return window_list
 
 # Define a function to extract features from a list of images
 # Have this function call bin_spatial() and color_hist()
@@ -176,92 +128,6 @@ def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
     # Return list of feature vectors
     return features
 
-# Define a function to extract features from a single image window
-# This function is very similar to extract_features()
-# just for a single image rather than list of images
-def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
-                        hist_bins=32, orient=9,
-                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
-                        spatial_feat=True, hist_feat=True, hog_feat=True):
-    # 1) Define an empty list to receive features
-    img_features = []
-    # 2) Apply color conversion if other than 'RGB'
-    if color_space != 'RGB':
-        if color_space == 'HSV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-        elif color_space == 'GRAY':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    else:
-        feature_image = np.copy(img)
-    # 3) Compute spatial features if flag is set
-    if spatial_feat == True:
-        spatial_features = bin_spatial(feature_image, size=spatial_size)
-        # 4) Append features to list
-        img_features.append(spatial_features)
-    # 5) Compute histogram features if flag is set
-    if hist_feat == True:
-        hist_features = color_hist(feature_image, nbins=hist_bins)
-        # 6) Append features to list
-        img_features.append(hist_features)
-    # 7) Compute HOG features if flag is set
-    if hog_feat == True:
-        if color_space == 'GRAY':
-            hog_features = get_hog_features(feature_image, orient,
-                                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        elif hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.extend(get_hog_features(feature_image[:, :, channel],
-                                                     orient, pix_per_cell, cell_per_block,
-                                                     vis=False, feature_vec=True))
-        else:
-            hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
-                                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        # 8) Append features to list
-        img_features.append(hog_features)
-
-    # 9) Return concatenated array of features
-    return np.concatenate(img_features)
-
-# Define a function you will pass an image
-# and the list of windows to be searched (output of slide_windows())
-def search_windows(img, windows, clf, scaler, color_space='RGB',
-                   spatial_size=(32, 32), hist_bins=32,
-                   hist_range=(0, 256), orient=9,
-                   pix_per_cell=8, cell_per_block=2,
-                   hog_channel=0, spatial_feat=True,
-                   hist_feat=True, hog_feat=True):
-    # 1) Create an empty list to receive positive detection windows
-    on_windows = []
-    # 2) Iterate over all windows in the list
-    for window in windows:
-        # 3) Extract the test window from original image
-        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
-        # 4) Extract features for that window using single_img_features()
-        features = single_img_features(test_img, color_space=color_space,
-                                       spatial_size=spatial_size, hist_bins=hist_bins,
-                                       orient=orient, pix_per_cell=pix_per_cell,
-                                       cell_per_block=cell_per_block,
-                                       hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                       hist_feat=hist_feat, hog_feat=hog_feat)
-        # 5) Scale extracted features to be fed to classifier
-        test_features = scaler.transform(np.array(features).reshape(1, -1))
-        # 6) Predict using your classifier
-        prediction = clf.predict(test_features)
-        # 7) If positive (prediction == 1) then save the window
-        if prediction == 1:
-            on_windows.append(window)
-    # 8) Return windows for positive detections
-    return on_windows
-
 
 def convert_color(img):
     # return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
@@ -274,6 +140,7 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell,
     boxes = []
     img_tosearch = img[ystart:ystop, :, :]
     ctrans_tosearch = convert_color(img_tosearch)
+
     if scale != 1:
         imshape = ctrans_tosearch.shape
         ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
@@ -290,6 +157,7 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell,
     # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
     window = 64
     nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
+
     cells_per_step = 1  # Instead of overlap, define how many cells to step
     nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
     nysteps = (nyblocks - nblocks_per_window) // cells_per_step
@@ -313,26 +181,23 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell,
             ytop = ypos * pix_per_cell
 
             # Extract the image patch
-            subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
+            subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
 
             # Get color features
             spatial_features = bin_spatial(subimg, size=spatial_size)
             hist_features = color_hist(subimg, nbins=hist_bins)
 
             # Scale features and make a prediction
-            test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
-            # test_features = X_scaler.transform(np.array(hog_features).reshape(1, -1))
+            test_features = X_scaler.transform( np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
+
             test_prediction = svc.predict(test_features)
 
-            # hog_features = X_scaler.transform(np.hstack((hog_feat1, hog_feat2, hog_feat3)).reshape(1, -1))
-            # test_prediction = svc.predict(hog_features)
-
+            # If positive (prediction == 1) then save the window
             if test_prediction == 1:
                 xbox_left = np.int(xleft * scale)
                 ytop_draw = np.int(ytop * scale)
                 win_draw = np.int(window * scale)
-                boxes.append(
-                    ((xbox_left, ytop_draw+ystart),(xbox_left+win_draw,ytop_draw+win_draw+ystart),(0,0,255),6))
+                boxes.append(((xbox_left, ytop_draw + ystart), (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255),6))
 
     return boxes
 
@@ -389,11 +254,6 @@ car_images = glob.glob('dataset/vehicles/**/*.png')
 noncar_images = glob.glob('dataset/non-vehicles/**/*.png')
 print(len(car_images), len(noncar_images))
 
-# Reduce the sample size because
-# The quiz evaluator times out after 13s of CPU time
-# sample_size = 500
-# car_images = car_images[0:sample_size]
-# noncar_images = noncar_images[0:sample_size]
 
 
 # t = time.time()
@@ -502,7 +362,7 @@ def process_video(img):
 
     heatmap_img = add_heat(img, boxes)
 
-    heatmap_img = apply_threshold(heatmap_img, 6 )
+    heatmap_img = apply_threshold(heatmap_img, 2 )
 
     labels = label(heatmap_img)
     draw_img = draw_labeled_bboxes(np.copy(img), labels)
@@ -510,17 +370,17 @@ def process_video(img):
     return draw_img
 
 
-# img = mpimg.imread('./test_images/test1.jpg')
+# img = mpimg.imread('./test_images/test5.jpg')
 # out = process_video(img)
 # plt.imshow(out)
 # plt.show()
-#
-# test_out_file2 = 'test_video_out.mp4'
-# clip_test2 = VideoFileClip('test_video.mp4')
-# clip_test_out2 = clip_test2.fl_image(process_video)
-# clip_test_out2.write_videofile(test_out_file2, audio=False)
 
-test_out_file2 = 'project_video_out.mp4'
-clip_test2 = VideoFileClip('project_video.mp4').subclip(20,28)
+test_out_file2 = 'test_video_out.mp4'
+clip_test2 = VideoFileClip('test_video.mp4')
 clip_test_out2 = clip_test2.fl_image(process_video)
 clip_test_out2.write_videofile(test_out_file2, audio=False)
+
+# test_out_file2 = 'project_video_out.mp4'
+# clip_test2 = VideoFileClip('project_video.mp4')#.subclip(20,28)
+# clip_test_out2 = clip_test2.fl_image(process_video)
+# clip_test_out2.write_videofile(test_out_file2, audio=False)
